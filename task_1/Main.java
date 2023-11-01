9603429183c730b5aa499a5cb465862dea6417ec
Emanuil <eiv.razgrad@gmail.com>
2023-11-01 20:39:51 +0200

    цикъл отпечатва 20 пъти Разград with while

--- task_1/Main.java
+++ task_1/Main.java
@@ -1,4 +1,17 @@
 package task_1;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        var i =0;
+        while (true){
+
+            System.out.println("Разград "+i);
+            i++;
+            if (i == 20) {
+                return;
+            }
+        }
+    }
 }
