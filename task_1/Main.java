03661a7cbba8bf8b7feee7196b9948b7e99fe745
Emanuil <eiv.razgrad@gmail.com>
2023-11-01 20:33:32 +0200

    цикъл отпечатва 20 пъти Разград

--- task_1/Main.java
+++ task_1/Main.java
@@ -1,4 +1,9 @@
 package task_1;
 
 public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Разград - "+i);
+        }
+    }
 }
