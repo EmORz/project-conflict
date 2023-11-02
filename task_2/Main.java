8daad426749f1653ba2d968f6ae0653dd0f6dd47
Emanuil <eiv.razgrad@gmail.com>
2023-11-02 21:01:13 +0200

    Ismail make branch conflict_6.

--- task_2/Main.java
+++ task_2/Main.java
@@ -3,5 +3,6 @@ package task_2;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Conflict Emo");
+        System.out.println("Conflict Ismail");
     }
 }
